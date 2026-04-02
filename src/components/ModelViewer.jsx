b3ff13f4b58ccbe4a91493efbbd48960c6fdc1ba
No_otra_vez <saulbector@gmail.com>
2026-04-02 15:28:32 -0600

    Add basic model viewer with dependencies

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -2,27 +2,48 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF, Environment, Center } from "@react-three/drei";
 import { Suspense } from "react";
 
-function Model({ url }) {
-    const { scene } = useGLTF(url);
-    return <primitive object={scene} />;
+function Model({ url, position = [0, 0, 0] }) {
+  const { scene } = useGLTF(url);
+  return (
+    <group position={position}>
+      <Center>
+        <primitive object={scene} />
+      </Center>
+    </group>
+  );
 }
 
-export default function ModelViewer({ model }) {
-    return (
-        <div className="w-125 h-125 bg-neutral-900 rounded-xl overflow-hidden">
-            <Canvas camera={{ position: [-0.15, 0.15, 0.15], fov: 45 }}>
-                <ambientLight intensity={1} />
-                <directionalLight position={[5, 5, 5]} intensity={1} />
+export default function ModelViewer({ model, model2 }) {
+  const hasTwoModels = model && model2;
 
-                <Suspense fallback={null}>
-                    <Center>
-                        <Model url={model} />
-                    </Center>
-                    <Environment preset="studio" environmentIntensity={0.3}/>
-                </Suspense>
+  return (
+    <div className="w-125 h-125 bg-neutral-900 rounded-xl overflow-hidden">
+      <Canvas camera={{ position: [0, 0.3, 0.5], fov: 35 }}>
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
 
-                <OrbitControls enableDamping />
-            </Canvas>
-        </div>
-    );
+        <Suspense fallback={null}>
+          {/* Modelo principal */}
+          {model && (
+            <Model
+              url={model}
+              position={hasTwoModels ? [0, 0, 0] : [0, 0, 0]}
+            />
+          )}
+
+          {/* Segundo modelo */}
+          {model2 && (
+            <Model
+              url={model2}
+              position={[0, 0.125, -0.032]}
+            />
+          )}
+
+          <Environment preset="studio" environmentIntensity={0.3}/>
+        </Suspense>
+
+        <OrbitControls enableDamping target={[0, 0.06, 0]}/>
+      </Canvas>
+    </div>
+  );
 }
